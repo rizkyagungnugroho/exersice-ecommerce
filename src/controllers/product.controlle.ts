b2793dd6b8002ce6efe324052e91b698dd8ceac9
Rizky Agung <48451302+rizkyagungnugroho@users.noreply.github.com>
2025-04-11 13:44:12 +0700

    Update product (#4)
    * setup db connection
    
    * feat : update-product

--- src/controllers/product.controlle.ts
+++ src/controllers/product.controlle.ts
@@ -1,5 +1,6 @@
 import { NextFunction,Request, Response } from "express"
 import { createProductService } from "../services/product/create-product.service";
+import { updateProductService } from "../services/product/update-product.service";
 
 
 export const createProductController = async  (
@@ -9,6 +10,19 @@ export const createProductController = async  (
 ) => {
     try{
         const result = await createProductService(req.body);
+        res.status(200).send(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateProductController = async  (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try{
+        const result = await updateProductService(Number(req.params.id), req.body);
         res.status(200).send(result);
     } catch (error) {
         next(error);
--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { createProductController } from "../controllers/product.controlle";
+import { 
+    createProductController, updateProductController 
+} from "../controllers/product.controlle";
 import { veriftyToken } from "../lib/jwt";
 import {veriftyRole} from "../middlewares/role.middleware"
 
 const router= Router ();
 
-router.post("/",veriftyToken, veriftyRole(["ADMIN"])    ,createProductController);
+router.post("/",veriftyToken, veriftyRole(["ADMIN"]) ,createProductController);
+router.patch("/:id",veriftyToken, veriftyRole(["ADMIN"]) ,updateProductController);
 
 export default router;
